e1e0cdfa37a8ed38af3978e274bd1e119d7f64ee
VLadFurmanyuk <furmanyukvlad13@gmail.com>
2022-09-06 12:40:30 +0300

    Feat

---
+++ script/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function (){
+    $('.slider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
+        // cssEase: 'cubic-bezier(0.600, -0.1, 0.555, 0.045)',
+        // easing: 'easeOutElastic',
+        // cssEase: 'ease-out',
+        responsive:[
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 3,
+                    autoplaySpeed: 4000,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 3,
+                    autoplaySpeed: 4000,
+                }
+            },
+            {
+                breakpoint: 321,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    autoplaySpeed: 4000,
+                }
+            },
+            {
+                breakpoint: 281,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    autoplaySpeed: 4000,
+                }
+            }
+        ]
+    });
+});
+
+// On before slide change
+$('.slider-nav').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.slick-slide').removeClass('slick-active-first slick-active-last');
+    $('.slick-active').eq(0).addClass('slick-active-first');
+    $('.slick-active').eq(3).addClass('slick-active-last');
+}).trigger('afterChange');
